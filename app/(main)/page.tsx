49da5bbc951585a94e6013bb45e2d731ccabd755
jumaniyozov <jumaniyozovse@gmail.com>
2023-06-19 09:30:14 +0500

    arangement changes

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,13 +5,13 @@ import Footer from "@/app/(main)/components/Footer";
 export default function Home() {
     return (
         <main className="flex h-full flex-col">
-            <div className="w-full xl:h-[480px]">
-                <GeneralMap/>
-            </div>
-            <div className="w-8/12 mx-auto h-full">
+            <div className="w-8/12 mx-auto xl:h-[260px]">
                 <Filters/>
             </div>
-            <div className="bg-brandDark-900 w-full h-96 mt-12">
+            <div className="w-full xl:h-[640px]">
+                <GeneralMap/>
+            </div>
+            <div className="bg-brandDark-900 w-full h-96">
                 <div className="w-8/12 mx-auto h-full">
                     <Footer/>
                 </div>
